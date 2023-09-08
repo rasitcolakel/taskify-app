481a728eed913c29b6de4dfc9a97008488a55a1f
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-09-09 00:02:17 +0300

    eslint, reanimated, prettier and some configurations added

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,22 @@
-module.exports = function (api) {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@assets': './assets',
+            '@src': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
